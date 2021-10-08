17fb48f828565ec1e259b243b27f19bd3d2202ae
Shubham Tomar <shubham.tomar@nagarro.com>
2021-10-08 12:23:16 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
        Development is gone here....
        Welcome to shubham tomar world
+       I am a final year undergrad Computer Science Engineering student and I am a good Competitive Programmer and having strong knowledge of Data Structures and Algorithms, Database, Computer Networking and Operating System and I have a field of interest in Computer Vision, Artificial Intelligence, Programming languages and Formal Methods.
       </header>
     </div>
   );
