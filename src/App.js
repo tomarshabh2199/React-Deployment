a7469b69c744538f8b72801d4228ad9a926ced7d
Shubham Tomar <shubham.tomar@nagarro.com>
2021-10-08 12:13:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        Development is gone here....
+       Welcome to shubham tomar world
       </header>
     </div>
   );
